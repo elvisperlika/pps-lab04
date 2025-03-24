fae5783f0254b51f0adc426e9cb858cdd834be2b
noodles <elvis.perlika@studio.unibo.it>
2025-03-24 11:26:25 +0100

    feat: complete exercise 1

--- src/main/scala/tasks/adts/Ex1ComplexNumber.scala
+++ src/main/scala/tasks/adts/Ex1ComplexNumber.scala
@@ -1,5 +1,7 @@
 package tasks.adts
 
+import scala.math.abs
+
 /*  Exercise 1: 
  *  Complete the implementation of ComplexADT trait below, so that it passes
  *  the test in ComplexTest.
@@ -19,12 +21,30 @@ object Ex1ComplexNumbers:
 
   object BasicComplexADT extends ComplexADT:
 
+    case class ComplexNumber(re: Double, im: Double)
     // Change assignment below: should probably define a case class and use it?
-    type Complex = Nothing 
-    def complex(re: Double, im: Double): Complex = ???
+    type Complex = ComplexNumber
+    def complex(re: Double, im: Double): Complex = ComplexNumber(re, im)
     extension (complex: Complex)
-      def re(): Double = ???
-      def im(): Double = ???
-      def sum(other: Complex): Complex = ???
-      def subtract(other: Complex): Complex = ???
-      def asString(): String = ???
+      def re(): Double = complex match
+        case ComplexNumber(re, _) => re
+
+      def im(): Double = complex match
+        case ComplexNumber(_, im) => im
+
+      def sum(other: Complex): Complex = (complex, other) match
+        case (ComplexNumber(re1, im1), ComplexNumber(re2, im2)) => ComplexNumber(re1 + re2, im1 + im2)
+
+      def subtract(other: Complex): Complex = (complex, other) match
+        case (ComplexNumber(re1, im1), ComplexNumber(re2, im2)) => ComplexNumber(re1 - re2, im1 - im2)
+
+      def asString(): String = complex match
+        case ComplexNumber(0, 0) => "0.0"
+        case ComplexNumber(re, 0) => re + ""
+        case ComplexNumber(0, im) => im match
+          case im if im < 0 => "-" + abs(im) + "i"
+          case _ => im + "i"
+        case ComplexNumber(re, im) => im match
+          case im if im < 0 => re + " - " + abs(im) + "i"
+          case _ => re + " + " + im + "i"
+
